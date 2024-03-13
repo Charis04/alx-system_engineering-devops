da4bdba18cf062e608005183a55b4fa8846558c2
Charis <charisadu@gmail.com>
2024-03-13 02:08:27 +0000

    scan log file for sender, receiver and flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,3 @@
 #!/usr/bin/env ruby
 
-log_line = ARGV[0]
-matches = log_line.scan(/\[from:([^[\]]+)\] \[to:([^[\]]+)\] \[flags:([^[\]]+)\]/)
-
-sender = matches[0][0]
-receiver = matches[0][1]
-flags = matches[0][2]
-
-puts "#{sender},#{receiver},#{flags}"
+puts ARGV[0].scan(/\[from:([^[\]]+)\] \[to:([^[\]]+)\] \[flags:([^[\]]+)\]/)
