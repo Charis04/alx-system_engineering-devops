7e35ce59e9cfed11f99375ba892f8fe1da64477e
Charis <charisadu@gmail.com>
2024-06-11 03:07:16 +0100

    use api to get employee tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,9 +4,9 @@ Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 if __name__ == "__main__":
+    import requests
     import sys
     import urllib
-    import requests
 
     if len(sys.argv) < 2:
         exit
