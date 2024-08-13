07e95d710563e2c0fe8e4c07acc13d4d4d540c47
Charis <charisadu@gmail.com>
2024-08-14 00:43:23 +0100

    advanced of advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -29,7 +29,9 @@ def count_words(subreddit, word_list, after=None, word_count={}):
     params = {'after': after} if after else {}
 
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+            )
         if response.status_code != 200:
             return
 
@@ -52,7 +54,9 @@ def count_words(subreddit, word_list, after=None, word_count={}):
             return count_words(subreddit, word_list, after, word_count)
         else:
             # Sort and print results after recursion is done
-            sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+            sorted_word_count = sorted(
+                word_count.items(), key=lambda x: (-x[1], x[0])
+                )
             for word, count in sorted_word_count:
                 if count > 0:
                     print(f"{word}: {count}")
