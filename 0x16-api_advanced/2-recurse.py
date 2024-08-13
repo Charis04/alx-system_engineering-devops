faa79f99186c6bf36b135fb9c3746d4ee0fc7cfc
Charis <charisadu@gmail.com>
2024-08-14 00:35:06 +0100

    fix pep 8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -14,18 +14,22 @@ def recurse(subreddit, hot_list=[], after=None):
 
     Args:
     subreddit (str): The name of the subreddit to query.
-    hot_list (list): The list that stores the titles of hot articles (used in recursion).
-    after (str): The "after" parameter used for pagination (used in recursion).
+    hot_list (list): The list that stores the titles of hot articles
+    (used in recursion). after (str): The "after" parameter used for
+    pagination (used in recursion).
 
     Returns:
-    list: A list of titles of hot articles, or None if the subreddit is invalid or no results are found.
+    list: A list of titles of hot articles, or None if the subreddit is
+    invalid or no results are found.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after} if after else {}
 
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+            )
         if response.status_code == 200:
             data = response.json()
             articles = data['data']['children']
